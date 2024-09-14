de94b592a9a19184388b159094622a5f67065647
SHANKAR VP <63314266+itsisthatis@users.noreply.github.com>
2024-09-14 15:14:17 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Define build directories
-SOURCE_DIR="src"
+SOURCE_DIR="."
 BUILD_DIR="build"
 
 # Clean previous builds
@@ -12,21 +12,14 @@ echo "Cleaning previous builds..."
 rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
 
-# Copy HTML files to the build directory
-echo "Copying HTML files..."
+# Copy all relevant files to the build directory
+echo "Copying files to build directory..."
 cp -r $SOURCE_DIR/*.html $BUILD_DIR/
-
-# Copy JavaScript files to the build directory
-echo "Copying JavaScript files..."
-cp -r $SOURCE_DIR/js $BUILD_DIR/
-
-# Copy audio and video files to the build directory
-echo "Copying media files..."
-cp -r $SOURCE_DIR/media $BUILD_DIR/
-
-# Copy images to the build directory
-echo "Copying images..."
-cp -r $SOURCE_DIR/images $BUILD_DIR/
+cp -r $SOURCE_DIR/*.js $BUILD_DIR/
+cp -r $SOURCE_DIR/*.mp3 $BUILD_DIR/ # Assuming audio files are mp3
+cp -r $SOURCE_DIR/*.png $BUILD_DIR/ # Assuming images are png
+cp -r $SOURCE_DIR/*.jpg $BUILD_DIR/ # Assuming images are jpg
+cp -r $SOURCE_DIR/*.gif $BUILD_DIR/ # Assuming images are gif
 
 # Optionally, you can include other tasks like:
 # - Minifying CSS files
@@ -34,10 +27,10 @@ cp -r $SOURCE_DIR/images $BUILD_DIR/
 
 # Example: Minify CSS files using a tool like cssnano (if installed)
 # echo "Minifying CSS files..."
-# find $BUILD_DIR/css -name "*.css" -exec cssnano {} {} \;
+# find $BUILD_DIR -name "*.css" -exec cssnano {} {} \;
 
 # Example: Minify JavaScript files using a tool like terser (if installed)
 # echo "Minifying JavaScript files..."
-# find $BUILD_DIR/js -name "*.js" -exec terser {} --compress --mangle -o {} \;
+# find $BUILD_DIR -name "*.js" -exec terser {} --compress --mangle -o {} \;
 
 echo "Build complete."
